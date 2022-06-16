ba8ba4c38b813a2b846a9c16253c40f40fc3a376
LeeFowlow-webonise <107060935+LeeFowlow-webonise@users.noreply.github.com>
2022-06-16 12:12:46 -0230

    useWidget error fixed, uncommented.

--- src/hooks/useWidget.js
+++ src/hooks/useWidget.js
@@ -12,57 +12,47 @@ import React from 'react';
  //     var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();</script>
 
 const useWidget = (cityId) => {
-    console.log(cityId);
-    // let widgetParamObj = {
-    //     id: 11,
-    //     cityid: cityId,
-    //     appid: process.env.REACT_APP_API_KEY,
-    //     units: 'metric',
-    //     containerid: 'useWidget-11'
-    // };
+    let widgetParamObj = {
+        id: 11,
+        cityid: cityId,
+        appid: process.env.REACT_APP_API_KEY,
+        units: 'metric',
+        containerid: 'useWidget-11'
+    };
 
-    // let widgetFunction = () => {
-    //     var script = React.createElement('script');
-    //     script.async = true;
-    //     script.charset = "utf-8";
-    //     script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-    //     var s = document.getElementsByTagName('script')[0];
-    //     s.parentNode.insertBefore(script, s);
-    // };
+    let widgetFunction = () => {
+        var script = React.createElement('script');
+        script.async = true;
+        script.charset = "utf-8";
+        script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+    };
 
-    // let mainScript = React.createElement(
-    //     'script',
-    //     {src: '//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'}
-    // );
+    let mainScript = React.createElement(
+        'script',
+        {src: '//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'}
+    );
 
-    // let callScript = React.createElement(
-    //     'script',
-    //     null,
-    //     `window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
-    //     window.myWidgetParam.push(${widgetParamObj});(${widgetFunction})
-    //     )();</script>`
+    let callScript = React.createElement(
+        'script',
+        null,
+        `window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
+        window.myWidgetParam.push(${widgetParamObj});(${widgetFunction})
+        )();</script>`
 
-    // );
+    );
 
     return(
-        'j'
-        // console.log([
-        //     {'wPO': widgetParamObj},
-        //     {'main': mainScript},
-        //     {'call': callScript},
-        //     {'func': widgetFunction}
-        // ])
+        console.log([
+            {'wPO': widgetParamObj},
+            {'main': mainScript},
+            {'call': callScript},
+            {'func': widgetFunction}
+        ])
         // mainScript,
         // callScript
     )
 }
 
 export default useWidget
-
-`<script src='//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/d3.min.js'></script>
-     <script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
-     window.myWidgetParam.push(widgetParamObj);
-     (function() {var script = document.createElement('script');script.async = true;
-     script.charset = "utf-8";
-     script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-     var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();</script>`
