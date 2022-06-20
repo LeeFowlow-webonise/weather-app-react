d97acd0f9491865eeb78ca058d6554d3cf964d3f
LeeFowlow-webonise <107060935+LeeFowlow-webonise@users.noreply.github.com>
2022-06-20 10:22:01 -0230

    App background changes based on current weather

--- src/hooks/useBackground.js
+++ src/hooks/useBackground.js
@@ -1,12 +1,3 @@
-
-
-function variableParser(variable){
-    let newVar = variable.replace(/\s+/g, '');
-
-    return newVar
-}
-
-//this function always returns the clear sky image path as of now
 const useBackground = (weatherData) => {
     const images = {
         brokenclouds: 'assets/images/backgrounds/broken\ clouds.jpg',//eslint-disable-line
@@ -19,15 +10,7 @@ const useBackground = (weatherData) => {
         mist: 'assets/images/backgrounds/mist.jpg',//eslint-disable-line        
     }
 
-
     let currentWeather = (weatherData[0].weather[0].description).replace(/\s+/g, '');
-    // let varChange = currentWeather.replace(/\s+/g, '');
-    //varChange = currentWeather;
-    // currentWeather = currentWeather.replace(/\s+/g, '');
-    // let currentWeather = variableParser(weatherData[0].weather[0].description);
-    
-
-    //not handling the variables properly. Converting it a string?
 
    
     if (images[currentWeather] != null){
